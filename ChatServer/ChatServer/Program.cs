fff2460edb8d4c4c9d65e9b009cb4296464e2e52
Bartomeu Mas <bartomeumas00@gmail.com>
2020-08-13 21:54:50 -0400

    Add files via upload

---
+++ ChatServer/ChatServer/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+using System.Collections;
+
+namespace ChatServer
+{
+    class Program
+    {
+        public static Hashtable clientsList = new Hashtable();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting CP server...");
+            int port = 1234;
+            TcpListener listener = new TcpListener(IPAddress.Loopback, port);
+            listener.Start();
+
+            TcpClient client = listener.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+            StreamWriter writer = new StreamWriter(stream, Encoding.ASCII) { AutoFlush = true };
+            StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+
+            while ((true))
+            {
+                byte[] bytesFrom = new byte[10025];
+                string dataFromClient = null;
+                stream.Read(bytesFrom, 0, (int)client.ReceiveBufferSize);
+                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+                clientsList.Add(dataFromClient, client);
+
+                broadcast(dataFromClient + " Joined ", dataFromClient, false);
+
+                Console.WriteLine(dataFromClient + " Joined chat room");
+            }
+        }
+
+        private static void broadcast(string msg, string uName, bool flag)
+        {
+           foreach(DictionaryEntry Item in clientsList)
+            {
+                TcpClient broadcastSocket;
+                broadcastSocket = (TcpClient)Item.Value;
+                NetworkStream broadcastStream = broadcastSocket.GetStream();
+                Byte[] broadcastBytes = null;
+
+                if (flag == true)
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(uName + " says : " + msg);
+                }
+                else
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(msg);
+                }
+
+                broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length);
+                broadcastStream.Flush();
+            }
+        }
+    }
+}
